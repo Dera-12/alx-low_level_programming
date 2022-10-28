fea88383ec68232ba0aee66b0eece111fe412a7b
Dera-12 <jessicachidera21@gmail.com>
2022-10-28 10:23:38 -0700

    concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat -concatenates two strings
+ * @str1: destination string
+ * @str2: source string
+ *
+ * Return:  value of the string after concentation
+ */
+char *_strcat(char *dest, char *src)
+{
+	char str1[98] = "Hello ";
+	char str2[] = "World!\n";
+
+	strcat(str1, str2);
+	_putchar(str1);
+	_putchar('\n');
+}
