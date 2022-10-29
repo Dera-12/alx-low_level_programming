63c42a28f6efdbf4c2c46ca335536077e29a45a1
Dera-12 <jessicachidera21@gmail.com>
2022-10-28 18:31:08 -0700

    lower to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s: change string
+ *
+ * Return: character
+ */
+char *string_toupper(char *s)
+{
+	int k;
+
+	k = 0;
+
+	while (*(s + k))
+	{
+		if (*(s + k) >= 'a' && *(s + k) <= 'z')
+				
+			       	*(s + k) -= 'a' - 'A';
+				k++;
+				}
+				return (s);
+				}
