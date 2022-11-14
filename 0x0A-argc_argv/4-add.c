9b061ffc29aa517628def6e17fa9f7909cb0c00d
Dera-12 <jessicachidera21@gmail.com>
2022-11-14 00:02:00 -0800

    Addition of positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main -add 2 positive numbers and print the result
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
  * Return: 1 if error, 0 if function runs properly.
  */
 int main(int argc, char *argv[])
