eb0af754aa0e29aa2e4e1d774ae56b2b5aacba29
Dera-12 <jessicachidera21@gmail.com>
2022-11-18 17:54:13 -0800

    searches an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -searches for an integers
+ * @array: array of integers
+ * @size: size of arrays
+ * @cmp: pointers to function used to compare va
+ * lues
+ *
+ * Return: If no element matches, return -1
+ * If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int z;
+	
+	if (array && cmp)
+		
+		for (z = 0; z < size; z++)
+			if (cmp(array[z]))
+				return (z);
+	
+	return (-1);
+}
